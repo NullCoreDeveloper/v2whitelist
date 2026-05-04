017338457f68a655fb37f38fd02a3bb50d56bab1
Nethunter <119652843+NullCoreDeveloper@users.noreply.github.com>
2026-05-04 16:27:01 +0500

    Release: Bump version to 1.0.3 (v711)

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kiktor.v2whitelist"
         minSdk = 24
         targetSdk = 36
-        versionCode = 710
-        versionName = "2.1.0"
+        versionCode = 711
+        versionName = "1.0.3"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
