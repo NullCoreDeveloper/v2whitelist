98036c4b42ffd70edb85c4777461e4fc2379ed05
Nethunter <119652843+NullCoreDeveloper@users.noreply.github.com>
2026-09-04 22:21:21 +0500

    Fix missing resumeWithException import in V2WScannerEngine

--- V2rayNG/app/src/main/java/com/kiktor/v2whitelist/handler/V2WScannerEngine.kt
+++ V2rayNG/app/src/main/java/com/kiktor/v2whitelist/handler/V2WScannerEngine.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import kotlin.coroutines.resumeWithException
 
 object V2WScannerEngine {
 
